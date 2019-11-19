9758eb7cf28b4fbfa63aa97c59cbefa5738448e3
Lucas Coon <lucascoon@yahoo.com>
2019-11-19 13:46:49 -0600

    Added Math
    Fixed previously added functions, added comments for each function, and added the function to swap the bytes.
    NOTE: Currently too many global variables for an UNO, will need to work on reducing memory usage

--- Lidar.ino
+++ Lidar.ino
@@ -1,16 +1,34 @@
+/*
+ * Purpose is to Run an YDLidar X2 Lidar using the serial pin and a relay through an Arduino
+ * This closes relay, gathers data from the lidar, opens the relay 
+ * then sorts then maps the data recieved.
+ * This is then repeated as necessary
+ */
+
 const int DATASIZE = 100;
 int i = 0;
 int array[360][2];
 
+void intangles(); // Intermediate Angle Function Definition
+int angle(int x); // Angle Function Definition
+int math(int a, int b);
 
 //STARTING SIGNAL IS 7 BYTES
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  //Create a connection with the relay module
 
 }
 
+
+///////////////////////////////////////////////////////////////////////////////
+//The Following Loop will only be used to gather data via serial through the Lidar
+//To Gather the data Comment out the Second Loop() function and uncomment this one
+//////////////////////////////////////////////////////////////////////////////
+
+/*
 void loop() {
   // put your main code here, to run repeatedly:  
  //  Serial.read();
@@ -23,17 +41,30 @@ void loop() {
   }
  }
 }
-
+*/
+////////////////////////////////////////////////////////
+/*
+ * Sorting Function will consist of 
+ * "INPUT_SIZE" amount of Bytes, with the 
+ * first 2 Bytes denoting the beginning of the scan command (0xA55A)
+ * Then find the First Packet head 0x55AA, 
+ * then stepping through to gather the distant points from the point cloud
+ */
+////////////////////////////////////////////////////////
 /* To use this loop() coment out the top loop and remove the comment here
  * ----------------------------------------------------------------------------
- *
-int INPUT_SIZE = 2000;
+ */
+const int INPUT_SIZE = 2000; //Amount of bytes that will be sorted each time the relay is triggered
 int storage[INPUT_SIZE];
 bool processing = 0;
+//int sample_rate = 0;
+//int Start = 0;
+//int End = 0;
 
 void loop() {
   while (!processing)
   {
+    //Trigger the Relay Remove the serial availabe, once relay is triggered start storing data
     if(Serial.available())
     {
      for(int j = 0; j < INPUT_SIZE; j++)
@@ -54,34 +85,58 @@ void loop() {
   }
   while(processing)
   {
+    
     //This is where the processing code to "sort" the data through the array
   }
 }
-*/
 
-// Intermediate Angles Function Header
-void intangles(samplerate);
-// Intermediate Angles Function Definition
-samplerate = 40;
-void intangles(samplerate)
+
+///////////////////////////////////////////////////////////////////////////////
+//Function used to make the "intermediate Angles"
+//Pre: sample_rate, a global variable; array of int called angle of sample size
+//Post: Returns nothing but fills in array with intermediate angles
+//Future: Probably will merge into main loop() function, Need to inclu
+//////////////////////////////////////////////////////////////////////////////
+//void intangles()
+//{
+//  int x = sample_rate;
+  //for(int i=2; i<x; i++)
+  //{
+//    angle[i] = (((End-Start)/(x-1))*(i-1)+Start);
+ // }
+  //return;
+//}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//Function used to calculate the start and end angles
+//Pre: Requires an angle after calculated from the "math function"
+//Post: Returns the angle post calculations 
+//Future: will remain as is
+//////////////////////////////////////////////////////////////////////////////
+int angle(int x)
 {
-  int angle[x];
-
-  for(i=2; i<x; i++)
-  {
-    angle[i] = (((LSA-FSA)/(x-1))*(i-1)+FSA);
-  }
-  return;
+  int theta = 0;
+  theta = x >> 1;
+  theta = theta/64;
+  return theta;
 }
 
-// Calculate Angle Function Header (From LSB and MSB)
-int angle(x);
-// Calculate Angle Function Definition (From LSB and MSB)
-int angle(x)
-{
-  x >> 1;
-  x = x/64;
-  return x;
-}
 
+
+///////////////////////////////////////////////////////////////////////////////
+//Function used to place 2 Bytes in MSB, LSB order
+//Pre: 2 Bytes recieved from the lidar
+//Post: Returns a single int after being the bytes have been merged
+//Future: No changes seen to be neccessary
+//////////////////////////////////////////////////////////////////////////////
+int math(int a, int b)
+{
+  int storage = b;
+  Serial.println(b, BIN);
+  storage= b<<8;
+  Serial.println(storage, BIN);
+  storage = storage|a;
+  //Serial.println(value, BIN);
+  return storage;
 }
